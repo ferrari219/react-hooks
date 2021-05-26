edddbf9b5c0ab60455a7e530107a53cf404ded49
ferrari219@nate.com <ferrari219@nate.com>
2021-05-26 18:06:58 +0900

    기본 UI구성

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
 import React, { useState } from 'react';
 
-const content = [
+const tabContent = [
 	{
 		id: 0,
-		tab: 'Section 1',
-		content: 'I am the content of the Section 1',
+		tab: 'tab1',
+		content: 'This is tab1 content',
 	},
 	{
 		id: 1,
-		tab: 'Section 2',
-		content: 'I am the content of the Section 2',
+		tab: 'tab2',
+		content: 'This is tab2 content',
 	},
 ];
 
-const useTabs = (initialTab, allTabs) => {
-	const [currentIndex, setCurrentIndex] = useState(initialTab);
-
-	if (!allTabs || !Array.isArray(allTabs)) {
-		// 에러 안나게 처리
-		return;
-	}
-
-	// console.log(setCurrentIndex);
-	return {
-		currentItem: allTabs[currentIndex],
-		changeItem: setCurrentIndex,
-	};
-};
-
 const App = () => {
-	const { currentItem, changeItem } = useTabs(0, content);
-	// const tabs = useTabs(0);
-	// const tabs = useTabs(0, content);
 	return (
 		<div>
-			{content.map((item, index) => (
-				<button type="button" key={item.id} onClick={() => changeItem(index)}>
+			{tabContent.map((item) => (
+				<button type="button" key={item.id}>
 					{item.tab}
 				</button>
 			))}
-			<div>{currentItem.content}</div>
 		</div>
 	);
 };
