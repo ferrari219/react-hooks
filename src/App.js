1f914b169454562cbbba891d6b7f522510e281b3
ferrari219@nate.com <ferrari219@nate.com>
2021-05-26 15:43:53 +0900

    useInput

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
 	const [value, setValue] = useState(initialValue);
 	const onChange = (e) => {
-		console.log(e.target);
+		const {
+			target: { value },
+		} = e;
+		// const value = e.target.value;
+		let willUpdate = true;
+		if (typeof validator === 'function') {
+			willUpdate = validator(value);
+			console.log(validator(value));
+		}
+		if (willUpdate) setValue(value);
 	};
-	console.log({ value }, { onChange });
 	return { value, onChange };
 };
 
 const App = () => {
-	const name = useInput('test');
+	// const maxLen = (value) => value.length <= 10;
+	const maxLen = (value) => !value.includes('@');
+	const inptBox = useInput('M', maxLen);
 	return (
 		<div>
-			<form>
-				<input type="text" placeholder="Here" {...name} />
-				{/* value={name.value} onChange={name.onChange} */}
-			</form>
+			<input type="text" {...inptBox} />
+			{/* <input type="text" value={inptBox.value} onChange={inptBox.onChange} /> */}
 		</div>
 	);
 };
