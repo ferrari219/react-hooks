39605cc9a0c5a92c6718fb448d3e2f8deff026db
ferrari219@nate.com <ferrari219@nate.com>
2021-05-27 15:07:19 +0900

    useTabs 구성 후 문제점

--- src/App.js
+++ src/App.js
@@ -13,7 +13,14 @@ const content = [
 	},
 ];
 
+const useTabs = (initialTab) => {
+	const [currentIndex, setCurrentIndex] = useState(initialTab);
+
+	return { currentIndex }; //이렇게만 하면 0만 노출되므로 필요한 답을 얻을수 없음
+};
+
 const App = () => {
+	const tabs = useTabs(0);
 	return (
 		<div>
 			{content.map((item) => (
@@ -21,6 +28,7 @@ const App = () => {
 					{item.tab}
 				</button>
 			))}
+			<div>{tabs.currentIndex}</div>
 		</div>
 	);
 };
