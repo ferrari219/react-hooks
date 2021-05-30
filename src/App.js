91edddfdb2e748ed30164ee1e383dcfc6f6012a8
ferrari219@nate.com <ferrari219@nate.com>
2021-05-30 16:21:56 +0900

    완성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const App = () => {
-	const useTitle = (initialTitle) => {
-		const [title, setTitle] = useState(initialTitle);
-		const updateTitle = () => {
-			const htmlTitle = document.querySelector('title');
-			htmlTitle.innerText = title;
-		};
-		useEffect(updateTitle, [title]);
-		console.log({ title }, { setTitle });
-		return setTitle;
-		// return { setTitle }; //객체로 뺄지 그냥 사용할지는 내 선택
+	const useClick = (onClick) => {
+		const ref = useRef();
+		// console.log(element.current); // undefinded로 나옴 => 비동기 처리 필요
+		useEffect(() => {
+			const element = ref;
+			console.log(element.current); //<h1>hi</h1>
+			if (typeof onClick === 'function') {
+				console.log(element.current);
+				// componentDidMount, componentDidUpdate일때 실행
+				console.log('componentDidMount, componentDidUpdate');
+				if (element.current) {
+					//값이 있는건 확인했으니
+					element.current.addEventListener('click', onClick);
+				}
+			}
+			return () => {
+				console.log(element.current);
+				console.log('componentWillUnmount');
+				if (element.current) {
+					element.current.removeEventListener('click', onClick);
+					//unmount 되면 해당 이벤트가 삭제 되므로 클릭해도 더이상 실행되지 않음
+				}
+			};
+		}, [onClick]);
+		return ref;
 	};
-	const titleUpdater = useTitle('Loading...');
-	setTimeout(() => titleUpdater('Home'), 500);
-	// setTimeout(() => titleUpdater.setTitle('Home'), 500);
+	const sayHello = () => {
+		console.log('Hello');
+	}; //onclick에서 실행할 함수 제작
+	const h1Element = useClick(sayHello); //componentDidMount, componentDidUpdate
+	//const h1Element = useClick(sayHello); // componentWillUnmount
 	return (
 		<div>
-			<h1>hi</h1>
+			<h1 ref={h1Element}>hi</h1>
+			<h2>bye</h2>
 		</div>
 	);
 };
