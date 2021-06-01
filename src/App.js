f75f1f6bb247630a63fee4dc7762e950934e7261
ferrari219@nate.com <ferrari219@nate.com>
2021-06-01 17:00:46 +0900

    initialize

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 
 const App = () => {
-	const useFadeIn = (duration = 1, delay = 1) => {
-		const ref = useRef();
-		useEffect(() => {
-			const element = ref;
-			// console.log(element.current);
-			if (element.current && duration !== 'number') {
-				const { current } = element;
-				current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
-				current.style.opacity = 1;
-			}
-		}, []);
-		return {
-			//이렇게 변형하면 재활용가능해짐
-			ref: ref,
-			style: { opacity: 0 },
-		};
-	};
-	const fadeInH1 = useFadeIn();
-	return (
-		<div>
-			<h1 {...fadeInH1}>test</h1>
-		</div>
-	);
+	return <div>App</div>;
 };
 
 export default App;
