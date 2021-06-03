1b032a6d2fee1c1f58d2b3421086cd65443306bc
ferrari219@nate.com <ferrari219@nate.com>
2021-06-03 17:12:40 +0900

    initialize

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const App = () => {
-	const useNetwork = (onChange) => {
-		console.log(navigator.onLine); // true
-		const [status, setStatus] = useState(navigator.onLine);
-
-		const handleChange = () => {
-			if (typeof onChange === 'function') {
-				onChange(navigator.onLine);
-			}
-			setStatus(navigator.onLine);
-		};
-		useEffect(() => {
-			window.addEventListener('online', handleChange);
-			window.addEventListener('offline', handleChange);
-
-			//'cwum 일때 처리
-			return () => {
-				window.removeEventListener('online', handleChange);
-				window.removeEventListener('offline', handleChange);
-			};
-		}, []);
-		return status;
-	};
-	const handleNetworkChange = (online) => {
-		console.log(online ? 'Online' : 'Offline');
-	};
-	const onLine = useNetwork(handleNetworkChange);
-	return <div>{onLine ? 'online' : 'offline'}</div>;
+	return <div>.</div>;
 };
 
 export default App;
