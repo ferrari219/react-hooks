083f2318e2dc891c311e48438ed6e9e129bfa334
ferrari219@nate.com <ferrari219@nate.com>
2021-06-03 18:04:27 +0900

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-	return <div>.</div>;
+	const useScroll = () => {
+		const [state, setState] = useState({
+			x: 0,
+			y: 0,
+		});
+		const onScroll = () => {
+			// console.log('x', window.scrollX, 'y', window.scrollY);
+			setState({ x: window.scrollX, y: window.scrollY });
+		};
+		useEffect(() => {
+			console.log('effect');
+			window.addEventListener('scroll', onScroll);
+			return () => {
+				console.log('cleanup');
+				window.removeEventListener('scroll', onScroll);
+			};
+		}, [state]);
+		return state;
+	};
+	const { y } = useScroll();
+	return (
+		<div style={{ height: '1000vh' }}>
+			<h1 style={{ position: 'fixed', color: y > 100 ? 'red' : 'blue' }}>test</h1>
+		</div>
+	);
 };
 
 export default App;
