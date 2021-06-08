b45f81c91c29887f0bb03743a80a0f5117393a96
ferrari219@nate.com <ferrari219@nate.com>
2021-06-08 17:14:46 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import React from 'react';
 
 const App = () => {
-	const useNotification = (title, options) => {
-		if (!('Notification' in window)) {
-			return;
-		}
-		// useEffect(() => {}, []);
-		const fireNotif = () => {
-			if (Notification.permission !== 'granted') {
-				Notification.requestPermission().then((permission) => {
-					if (permission === 'granted') {
-						new Notification(title, options);
-					} else {
-						return;
-					}
-				});
-			} else {
-				new Notification(title, options);
-			}
-		};
-		return fireNotif;
-	};
-	const triggerNotif = useNotification('Can I allow this?');
-	return (
-		<div>
-			<button type="button" onClick={triggerNotif}>
-				click
-			</button>
-		</div>
-	);
+	return <div>.</div>;
 };
 
 export default App;
