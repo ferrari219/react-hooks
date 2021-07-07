302e8fda9f748f19106b85b13ae5aec4f8d50fa1
ferrari219@nate.com <ferrari219@nate.com>
2021-07-07 21:08:42 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React from 'react';
-import useAxios from './useAxios';
 
 const App = () => {
-	const { loading, error, data, refetch } = useAxios({
-		url: 'https://reqres.in/api/users?delay=1',
-	});
-	// console.log(loading, error, data);
-	console.log(`Loading:${loading}\n Error:${error}\n Data:${JSON.stringify(data)}`);
-	return (
-		<div>
-			<h1>{data && data.status}</h1>
-			<h2>{loading && 'Loading'}</h2>
-			<button type="button" onClick={refetch}>
-				refetch
-			</button>
-		</div>
-	);
+  return <div>App</div>;
 };
 
 export default App;
