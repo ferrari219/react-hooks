848f31cc915c31db32f24235a32691bf2367916c
ferrari219@nate.com <ferrari219@nate.com>
2021-07-08 18:02:25 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
 import Screen from './Screen';
-import UserContextProvider from './context';
 
 const App = () => {
-	// const handleLogin = (e) => {
-	// 	e.preventDefault();
-	// };
 	return (
-		<UserContextProvider>
+		<>
 			<Screen />
-			{/* <button onClick={handleLogin}>Log user in</button> */}
-		</UserContextProvider>
+		</>
 	);
 };
 
--- src/Headers.js
+++ src/Headers.js
@@ -1,15 +1,10 @@
 // import React, { useContext } from 'react';
 import React from 'react';
-import { useUser } from './context';
 
 const Headers = () => {
-	// const context = useContext(UserContext);
-	const { name, loggedIn } = useUser();
-	// console.log(name, loggedIn);
 	return (
 		<header>
-			<a href="#">Home</a> Hello, {name}! You are{' '}
-			{loggedIn ? 'loggedIn' : 'anonymous'}
+			<a href="#">Home</a> Hello, {name}!
 		</header>
 	);
 };
