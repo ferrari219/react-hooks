52b8a7f3199bfc0c5aceb4ae79f3d9e7254461e8
ferrari219@nate.com <ferrari219@nate.com>
2021-07-10 11:24:40 +0900

    Done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useReducer } from 'react';
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 const reducer = (state, action) => {
-  switch (action.type) {
+  switch (action) {
     case INCREMENT:
       return { count: state.count + 1 };
     case DECREMENT:
@@ -18,8 +18,8 @@ const App = () => {
   return (
     <>
       <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
-      <button onClick={() => dispatch({ type: DECREMENT })}>Remove</button>
+      <button onClick={() => dispatch(INCREMENT)}>Add</button>
+      <button onClick={() => dispatch(DECREMENT)}>Remove</button>
     </>
   );
 };
