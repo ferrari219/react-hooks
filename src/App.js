5a7bd757977ec0f11ebdb9a7f850fa0a6b051ec7
ferrari219@nate.com <ferrari219@nate.com>
2021-07-10 11:14:37 +0900

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
 
 const App = () => {
-  return <div>test</div>;
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  return (
+    <>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: INCREMENT })}>Add</button>
+      <button onClick={() => dispatch({ type: DECREMENT })}>Remove</button>
+    </>
+  );
 };
 
 export default App;
